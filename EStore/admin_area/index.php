74faff2af0342ce8378994d289bb2848b0af7e02
AmirSph <80755988+AmirSph@users.noreply.github.com>
2021-05-20 18:29:31 +0200

    estore

---
+++ EStore/admin_area/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> E-Store Admin Area </title>
+    <link rel="stylesheet" href="css/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div id="wrapper"><!--#wrapper Begin -->
+
+
+<?php include("includes/sidebar.php"); ?>
+
+
+    <div id="page-wrapper"><!--#page_wrapper Begin -->
+        <div class="container-fluid"><!--container-fluid Begin -->
+        
+        
+        
+        
+        
+        </div><!--container-fluid Finish -->
+    </div><!--#page_wrapper finish -->
+</div><!--wrapper finishi -->
+
+<script src="js/jquery-331.min.js"></script>
+<script src="js/bootstrap-337.min.js"></script>
+    
+</body>
+</html>
