978f5f80242fdc82fd564097ddd7a02dc161284d
AmirSph <80755988+AmirSph@users.noreply.github.com>
2021-05-20 18:19:05 +0200

    Add files via upload

--- EStore/customer/includes/sidebar.php
+++ EStore/customer/includes/sidebar.php
@@ -1,20 +1,51 @@
 <div class="panel panel-default sidebar-menu"><!--  panel panel-default sidebar-menu Begin  -->
     
     <div class="panel-heading"><!--  panel-heading  Begin  -->
-        
-        <center><!--  center  Begin  -->
-            
-            <img class=" profile" src="customer_images/erika.jpg" alt="Person Profile">
-            
-        </center><!--  center  Finish  -->
-        
+      
+      <?php
+
+      $customer_session = $_SESSION['customer_email'];
+      $get_customer = "select * from customers where customer_email = '$customer_session' ";
+      $run_customer = mysqli_query($con , $get_customer);
+      $row_customer = mysqli_fetch_array($run_customer);
+
+      $customer_image = $row_customer['customer_image'];
+      $customer_name = $row_customer['customer_name'];
+
+      if(!isset($_SESSION['customer_email']))
+      {
+
+
+      }else
+      {
+
+        echo "
+
+        <center>
+
+        <img src='customer_images/$customer_image' class= 'img-responsive'>
+
+        </center>
+
         <br/>
+
+        <h3 class='panel-title' align='center'> 
+
+        Name: $customer_name
+
         
-        <h3 align="center" class="panel-title"><!--  panel-title  Begin  -->
-            
-            Name: Mr Burhan
-            
-        </h3><!--  panel-title  Finish -->
+
+
+        
+        
+        </h3>
+        
+        
+        ";
+
+      }
+
+      ?>
         
     </div><!--  panel-heading Finish  -->
     
